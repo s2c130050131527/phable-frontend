01aec203629dbc00c1fe7721515744b6b425ae80
Naygandhi Shrey Sanjay <shrey.ns@external.swiggy.in>
2021-08-08 17:02:31 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+The frontend repo for this project is [enter link description here][2]{:target='_blank'}
+Running steps on local 
+
+ - yarn install
+ - yarn start
+
+Features implement
+
+
+  [1]: https://github.com/s2c130050131527/phable-backend
+  [2]: https://github.com/s2c130050131527/phable-frontend
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
+import { ChakraProvider } from "@chakra-ui/react"
+import { lazy, Suspense } from 'react';
+
+const NotFound = lazy(() => import('./components/NotFound'));
+const Home = lazy(() => import('./components/Home'));
+
+
+function App() {
+  return (
+    <Suspense fallback="...">
+      <ChakraProvider>
+        <BrowserRouter>
+          <div className="App">
+            <Switch>
+              <Route exact path="/" component={Home}></Route>
+              <Route component={Home}></Route>
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </ChakraProvider>
+    </Suspense>
+
+
+  );
+}
+
+export default App;
